f089f529044d71ca8b57640f749f60499859b52d
Patryk Pasek <patrykpasek@onet.pl>
2022-07-31 19:55:53 +0200

    Support for delete pod logic

---
+++ src/Bucket.UseCases/Commands/DeletePodCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Bucket.UseCases.Commands
+{
+	public class DeletePodCommand : INotification
+	{
+		public string Name { get; set; }
+
+		public string Namespace { get; set; }
+
+		public DeletePodCommand(string name, string @namespace)
+		{
+			Name = name;
+			Namespace = @namespace;
+		}
+	}
+}
---
+++ src/Bucket.UseCases/Commands/Handlers/DeletePodCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Bucket.UseCases.Services;
+using k8s;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bucket.UseCases.Commands.Handlers
+{
+    public class DeletePodCommandHandler : INotificationHandler<DeletePodCommand>
+	{
+        private readonly CurrentContextService _currentContextService;
+
+        public DeletePodCommandHandler(CurrentContextService currentContextService)
+        {
+            _currentContextService = currentContextService;
+        }
+
+        public async Task Handle(DeletePodCommand notification, CancellationToken cancellationToken)
+		{
+            await _currentContextService.GetContext()!.CoreV1.DeleteNamespacedPodAsync(notification.Name, 
+                notification.Namespace, 
+                cancellationToken: cancellationToken);
+        }
+	}
+}
